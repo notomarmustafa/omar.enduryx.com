fdb612955cbdce22f0bcfba50f803c1df8fada1c
Omar Mustafa <126339620+OmarMustafa24@users.noreply.github.com>
2023-07-08 23:13:25 -0400

    init

--- assists/js/script.js
+++ assists/js/script.js
@@ -1,7 +1,25 @@
 // toggle icon navbar
 
 //scroll sections
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
 window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            //active navbar links
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            });
+        }
+    });
+
     //sticky header
     let header = document.querySelector('header');
 
