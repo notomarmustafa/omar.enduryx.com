8e7f651e9f7c9d94f7a6f978273a31a1d2d04d3d
Omar Mustafa <126339620+OmarMustafa24@users.noreply.github.com>
2023-07-08 22:58:43 -0400

    init

---
+++ assists/js/script.js
@@ -0,0 +1,9 @@
+// toggle icon navbar
+
+//scroll sections
+window.onscroll = () => {
+    //sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
