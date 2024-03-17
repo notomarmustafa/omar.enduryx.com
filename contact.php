0188c8febd03de4c3e9891842ca29097eba7b11c
Omar Mustafa <126339620+OmarMustafa24@users.noreply.github.com>
2024-03-17 06:54:22 -0400

    init

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+
+if(isset($_POST['submit']))
+{
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $phone = $_POST['phone'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mailTo = "omarmustafa2024@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html?mailsent");
+}
+
+?>
