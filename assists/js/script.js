e82b3bf8894d6b255511bfe51bd56d39a7672dd8
Omar Mustafa <126339620+OmarMustafa24@users.noreply.github.com>
2024-03-17 07:45:54 -0400

    init

--- assists/js/script.js
+++ assists/js/script.js
@@ -58,3 +58,16 @@ window.addEventListener("load", () => {
         document.body.removeChild("loader");
     })
 })
+
+function SendMail() {
+    var params = {
+        from_name : document.getElementById("name").value,
+        email_id : document.getElementById("email").value,
+        phone : document.getElementById("phone").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value
+    }
+    emailjs.send("service_djh0lws", "template_k8jpgif", params).then(function (res) {
+        alert("Message Sent Successfully!" + res.status);
+    })
+}
