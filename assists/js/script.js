6d21580c78c4673bd70c7f6d6f1dcc36b2e5316e
Omar Mustafa <126339620+OmarMustafa24@users.noreply.github.com>
2024-04-08 22:35:16 -0400

    init

--- assists/js/script.js
+++ assists/js/script.js
@@ -1,73 +1,80 @@
 // toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
 //scroll sections
-let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset + height) {
-            //active navbar links
-            navlinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
-            });
-            // active sections for animation on scroll
-            sec.classList.add('show-animate');
-        }
-        // if want to use animation that repeats on scroll use this
-        else {
-            sec.classList.remove('show-animate');
-        }
-    });
+    if (top >= offset && top < offset + height) {
+      //active navbar links
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+      // active sections for animation on scroll
+      sec.classList.add("show-animate");
+    }
+    // if want to use animation that repeats on scroll use this
+    else {
+      sec.classList.remove("show-animate");
+    }
+  });
 
-    //sticky header
-    let header = document.querySelector('header');
+  //sticky header
+  let header = document.querySelector("header");
 
-    header.classList.toggle('sticky', window.scrollY > 100);
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-    //remove toggle icon and navbar when click navbar links (scroll)
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
+  //remove toggle icon and navbar when click navbar links (scroll)
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 
-    // animation footer on scroll
-    let footer = document.querySelector('footer');
+  // animation footer on scroll
+  let footer = document.querySelector("footer");
 
-    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
-}
+  footer.classList.toggle(
+    "show-animate",
+    this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight
+  );
+};
 
 // Loader
 window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader");
+  const loader = document.querySelector(".loader");
 
-    loader.classList.add("loader-hidden");
+  loader.classList.add("loader-hidden");
 
-    loader.addEventListener("transitionend", () => {
-        document.body.removeChild("loader");
-    })
-})
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild("loader");
+  });
+});
 
 function SendMail() {
-    var params = {
-        from_name : document.getElementById("name").value,
-        email_id : document.getElementById("email").value,
-        phone : document.getElementById("phone").value,
-        subject : document.getElementById("subject").value,
-        message : document.getElementById("message").value
-    }
-    emailjs.send("service_djh0lws", "template_k8jpgif", params).then(function (res) {
-        alert("Message Sent Successfully!" + res.status);
-    })
+  var params = {
+    from_name: document.getElementById("name").value,
+    email_id: document.getElementById("email").value,
+    phone: document.getElementById("phone").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs
+    .send("service_djh0lws", "template_k8jpgif", params)
+    .then(function (res) {
+      alert("Message Sent Successfully!" + res.status);
+    });
 }
